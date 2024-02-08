7f1f7dbf93ac2f3792775bf52672e97eaaf05e5d
marcel <marcel.resende@yahoo.com.br>
2024-02-07 23:20:52 -0300

    adicionado grocery e ion auth

--- app/Config/Services.php
+++ app/Config/Services.php
@@ -3,6 +3,7 @@
 namespace Config;
 
 use CodeIgniter\Config\BaseService;
+use Config\IonAuth;
 
 /**
  * Services Configuration file.
@@ -29,4 +30,12 @@ class Services extends BaseService
      *     return new \CodeIgniter\Example();
      * }
      */
+    public static function ionAuth($getShared = true)
+    {
+        if ($getShared) {
+            return static::getSharedInstance('ionAuth');
+        }
+
+        return new IonAuth();
+    }
 }
