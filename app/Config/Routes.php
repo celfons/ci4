c47930bc3005666e082c61d99dd3edbd40c3a4f0
marcel <marcel.resende@yahoo.com.br>
2024-02-12 08:42:51 -0300

    add rotas de admin

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -47,14 +47,13 @@ $routes->group('auth', ['namespace' => 'App\Controllers'], function ($routes) {
     $routes->add('login', 'Auth::login');
 	$routes->get('logout', 'Auth::logout');
 	$routes->add('forgot_password', 'Auth::forgot_password');
-	// $routes->get('/', 'Auth::index');
-	// $routes->add('create_user    ', 'Auth::create_user');
-	// $routes->add('edit_user/(:num)', 'Auth::edit_user/$1');
-	// $routes->add('create_group', 'Auth::create_group');
-	// $routes->get('activate/(:num)', 'Auth::activate/$1');
-	// $routes->get('activate/(:num)/(:hash)', 'Auth::activate/$1/$2');
-	// $routes->add('deactivate/(:num)', 'Auth::deactivate/$1');
-	// $routes->get('reset_password/(:hash)', 'Auth::reset_password/$1');
-	// $routes->post('reset_password/(:hash)', 'Auth::reset_password/$1');
-	// ...
+	$routes->get('/', 'Auth::index', ['filter' => 'authFilter']);
+	$routes->add('create_user', 'Auth::create_user', ['filter' => 'authFilter']);
+	$routes->add('edit_user/(:num)', 'Auth::edit_user/$1', ['filter' => 'authFilter']);
+	$routes->add('create_group', 'Auth::create_group', ['filter' => 'authFilter']);
+	$routes->get('activate/(:num)', 'Auth::activate/$1', ['filter' => 'authFilter']);
+	$routes->get('activate/(:num)/(:hash)', 'Auth::activate/$1/$2', ['filter' => 'authFilter']);
+	$routes->add('deactivate/(:num)', 'Auth::deactivate/$1', ['filter' => 'authFilter']);
+	$routes->get('reset_password/(:hash)', 'Auth::reset_password/$1', ['filter' => 'authFilter']);
+	$routes->post('reset_password/(:hash)', 'Auth::reset_password/$1', ['filter' => 'authFilter']);
 });
